c420db3770bf8c7089f1b917db20e3c86a85d36a
Ismael <lindoespinoso@gmail.com>
2023-03-13 09:10:47 +0100

    v.0.0.1

--- Upgradeable/contracts/skullV1.sol
+++ Upgradeable/contracts/skullV1.sol
@@ -2,30 +2,33 @@
 
 pragma solidity ^0.8.11;
 
+// import upgradeable version of the ERC20 contract
 import "@openzeppelin/contracts-upgradeable/token/ERC20/ERC20Upgradeable.sol";
+// import Ownable upgradeable so only the contract owner can run admin actions on the contract;
 import "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
+// import the open the Proxy contract
 import "@openzeppelin/contracts-upgradeable/proxy/utils/UUPSUpgradeable.sol";
 
-contract CuyTokenV2 is
+contract SkullV1 is
     ERC20Upgradeable,
     UUPSUpgradeable,
     OwnableUpgradeable
 {
+    // using an initializer instead of a contructor
     function initialize(uint256 initialSupply) public initializer {
-        __ERC20_init("CuyToken", "CUY");
+        // As we are not using constructors we have to use the ERC20's contract initializer as well
+        __ERC20_init("Skull", "CUY");
+        // init the ownable contract and proxy
         __Ownable_init_unchained();
         __UUPSUpgradeable_init();
+        // mint an initial amount of tokens
         _mint(msg.sender, initialSupply * (10**decimals()));
     }
 
+    // Override this function required for the proxy to work
     function _authorizeUpgrade(address newImplementation)
         internal
         override
         onlyOwner
     {}
-
-    // New function that can only be executed by the contract owner
-    function mint(address toAccount, uint256 amount) public onlyOwner {
-        _mint(toAccount, amount);
-    }
 }
